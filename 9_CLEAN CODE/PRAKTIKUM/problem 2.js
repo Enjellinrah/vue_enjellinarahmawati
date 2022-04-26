06ea0aeaaef94a8e4e002117531c0be8d351ef87
Enjellinrah <enjellinarahmawati@gmail.com>
2022-04-26 14:49:43 +0700

    praktikum clean code

---
+++ 9_CLEAN CODE/PRAKTIKUM/problem 2.js
@@ -0,0 +1,21 @@
+// PROBLEM 2 - REWRITE
+
+/* JAWABAN 
+
+const [formdata, setformdata] = useState9{
+    totalroda :'0',
+    kecepatanperjam :'0',
+});
+
+    const mobil = {
+        berjalan : "tambah kecepatan",
+        kecepatanperjam : "kecepatanperjam + kecepatan baru",
+    }
+
+void main
+    mobilcepat = new mobil();
+    mobilcepat.berjalan{};
+
+    mobillamban = new mobil();
+    mobillamban.berjalan{}
+*/
