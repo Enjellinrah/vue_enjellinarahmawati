8f649026b5031a378f223d7cc306af4b85569d73
Enjellinrah <enjellinarahmawati@gmail.com>
2022-05-10 17:43:55 +0700

    problrm 1

---
+++ 9_CLEAN CODE/PRAKTIKUM/problem 1.js
@@ -0,0 +1,23 @@
+// PROBLEM 1 - ANALYSIS
+
+/* JAWABAN 
+
+Terdapat banyak kekurangan didalam kode tersebut, diantaranya :
+1. Menggunakan huruf kecil dalam penamaan class nya. 
+Class seharusnya ditulis menggunakan awalan huruf besar. Apabila lebih dari 1 kata diikuti dengan huruf besar. 
+
+contohnya :
+a. class User {}
+b. class UserService{}
+
+2. Penamaan fungsi lebih baik diawali dengan huruf kecil dan apabila lebih dari satu kata diikuti dengan huruf besar.
+
+contohnya :
+a. getUserById(userId)
+b. getAllUsers
+
+//noted
+1. pakai Let atau const
+
+
+*/
