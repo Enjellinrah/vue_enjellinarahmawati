1172ecb85e4a0d6306b6443f22d1d21c5a4061db
Enjellinrah <enjellinarahmawati@gmail.com>
2022-05-11 20:17:21 +0700

    Praktikum Problem 2

--- 09_CLEAN CODE/PRAKTIKUM/problem 2.js
+++ 09_CLEAN CODE/PRAKTIKUM/problem 2.js
@@ -1,21 +1,31 @@
 // PROBLEM 2 - REWRITE
 
-/* JAWABAN 
+/* JAWABAN
 
-const [formdata, setformdata] = useState9{
-    totalroda :'0',
-    kecepatanperjam :'0',
-});
+class Kendaraan {                                      // Penulisan nama class diubah sesuai aturan PascalCase
+    totalRoda :0,                                      // Penulisan nama variabel diubah sesuai aturan camelCase
+    kecepatanPerJam :0,
+}
 
-    const mobil = {
-        berjalan : "tambah kecepatan",
-        kecepatanperjam : "kecepatanperjam + kecepatan baru",
+// Penulisan nama class diubah sesuai dengan aturan PascalCase
+class Mobil extends Kendaraan {
+
+    berjalan () {
+        tambahKecepatan(10);                         // Memanggil fungsi tambahKecepatan dengan argument 10,
     }
+    
+    tambahKecepatan(kecepatanBaru) {                 // Penulisan nama function diubah sesuai aturan camelCase
+        return kecepatanPerJam += kecepatanBaru     // Merpersingkat penulisan penjumlahan dengan operator penugasan "+="
+    }
+}
 
-void main
-    mobilcepat = new mobil();
-    mobilcepat.berjalan{};
+function main() {
+    // Penulisan nama variabel diubah sesuai aturan camelCase
+    const mobilCepat = new Mobil();                 // Menambahkan keyword "const";
+    mobilCepat.berjalan();                          //Pemanggilan fungsi cukup sekali
 
-    mobillamban = new mobil();
-    mobillamban.berjalan{}
-*/
+    Penulisan nama variabel diubah sesuai aturan camelCase
+    const mobilLamban = new Mobil();                // Menambahkan keyword "const";
+    mobilLamban.berjalan()
+}
+ */
