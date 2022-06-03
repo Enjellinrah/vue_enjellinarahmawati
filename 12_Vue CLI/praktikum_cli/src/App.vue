e542aca59267aaebfed194ce41a20578d6d0bbac
Enjellinrah <enjellinarahmawati@gmail.com>
2022-06-03 22:02:34 +0700

    praktikum Vue_CLI

---
+++ 12_Vue CLI/praktikum_cli/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <ol>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo }}
+      </li>
+    </ol>
+        <input type="text" v-model="newTodo"/>
+        <button @click="addTodo">Tambahkan</button>
+        <p v-if="this.todos.length >= 4">Hebat!</p>
+        <p v-if="this.todos.empty = false"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      newTodo: "",
+      todos:[],
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push(this.newTodo);
+      this.newTodo = "";
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin-left: 50px;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
