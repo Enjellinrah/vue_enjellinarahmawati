170ef8b003159bb201ab0e1ef0ad94211c2be593
Enjellinrah <enjellinarahmawati@gmail.com>
2022-06-04 08:37:09 +0700

    change color

--- 12_Vue CLI/praktikum_cli/src/App.vue
+++ 12_Vue CLI/praktikum_cli/src/App.vue
@@ -37,7 +37,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   margin-left: 50px;
-  color: #2c3e50;
+  color: black;
   margin-top: 60px;
 }
 </style>
