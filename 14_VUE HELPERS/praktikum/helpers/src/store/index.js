14dc3e766a9e6271f79eb073f926427df648847d
Enjellinrah <99709538+Enjellinrah@users.noreply.github.com>
2022-07-27 21:26:40 +0700

    update praktikum

--- 14_VUE HELPERS/praktikum/helpers/src/store/index.js
+++ 14_VUE HELPERS/praktikum/helpers/src/store/index.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
+import Vue from 'vue';
 import Vuex from 'vuex';
-import createPersistedState from 'vuex-persistedstate'
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex);
 
-const store = new Vuex.Store({
+export default new Vuex.Store({
   plugins: [createPersistedState()],
   state: {
     isEditing: false,
